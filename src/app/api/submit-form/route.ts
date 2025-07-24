5aeb4f22bb1251523f52ba9d9ea22301173fd738
Лев Мукосеев <dogn2000@gmail.com>
2025-07-24 20:50:40 +0500

    remove all DB integration, leave only email sending for requests

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -1,34 +1,9 @@
-import Redis from 'ioredis';
 import { NextResponse, NextRequest } from 'next/server';
 import nodemailer from 'nodemailer';
-
-// Проверяем, что переменная окружения доступна
-if (!process.env.REDIS_URL) {
-  throw new Error('Missing REDIS_URL environment variable');
-}
-
-// Создаем клиент один раз. Он будет переиспользоваться между вызовами.
-const redisClient = new Redis(process.env.REDIS_URL);
 
 export async function POST(request: NextRequest) {
   try {
     const formData = await request.json();
-    const id = `req_${Date.now()}_${Math.random().toString(36).substring(2, 9)}`;
-
-    const requestData = {
-      ...formData,
-      id,
-      createdAt: new Date().toISOString(),
-      status: 'pending',
-    };
-
-    const dataForHash = Object.entries(requestData).flatMap(([key, value]) => [key, String(value)]);
-    
-    await redisClient.hset(`request:${id}`, ...dataForHash);
-    await redisClient.zadd('requests_by_date', Date.now(), `request:${id}`);
-
-    console.log('Successfully saved new request:', requestData);
-
     // Отправка письма
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
@@ -39,7 +14,7 @@ export async function POST(request: NextRequest) {
         pass: process.env.SMTP_PASS,
       },
     });
-    var str_data = `Поступила новая заявка:
+    var str_data = `Поступила новая заявка:\n
     Материал: ${formData["material"]}
     Операция: ${formData["operation"]}
     Оборудование: ${formData["machine"]}
@@ -57,9 +32,7 @@ export async function POST(request: NextRequest) {
       subject: `Новая заявка от ${formData["name"]}`,
       text: str_data
     });
-
-    return NextResponse.json({ message: 'Заявка успешно создана', id }, { status: 201 });
-
+    return NextResponse.json({ message: 'Заявка успешно создана' }, { status: 201 });
   } catch (error) {
     console.error('Error processing request:', error);
     const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
