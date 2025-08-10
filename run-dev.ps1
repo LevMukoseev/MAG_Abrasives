8220a421c8eb405a05f1117cf121042fea8558f6
LevMukoseev <your.email@example.com>
2025-08-10 23:34:13 +0500

    изменение шапки

--- run-dev.ps1
+++
@@ -1,5 +0,0 @@
-$env:PATH = "C:\Users\PREDATOR\huinya;" + $env:PATH
-Write-Host "Installing dependencies..."
-& "C:\Users\PREDATOR\huinya\npm.cmd" install
-Write-Host "Starting development server..."
-& "C:\Users\PREDATOR\huinya\npm.cmd" run dev 
